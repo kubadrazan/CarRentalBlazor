2e86d3b9927700f84bf01890bfd96720fd83502e
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2024-11-09 01:44:41 +0100

    Service and CarToRentDetails Page. Car and offer classes temporary

---
+++ MiNICarRentalBrowser/Data/Offer.cs
@@ -0,0 +1,8 @@
+﻿namespace MiNICarRentalBrowser.Data
+{
+    public class Offer // TODO
+    {
+        public string text { get; set; }
+        public float price { get; set; }
+    }
+}
---
+++ MiNICarRentalBrowser/Services/RentalServicecs.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using MiNICarRentalBrowser.Data;
+
+namespace Browser_FrontEnd.Services
+{
+    public class RentalServicecs
+	{
+		private readonly HttpClient _httpClient;
+		private readonly string _url;
+		public RentalServicecs(HttpClient httpClient, IConfiguration configuration)
+		{
+			{
+				_httpClient = httpClient;
+				_url = configuration.GetValue<string>("ApiSettings:Url");
+			}
+		}
+		public async Task<Car> GetCarDetailsAsync(int carId)
+		{
+			try
+			{
+				var response = await _httpClient.GetFromJsonAsync<Car>($"{_url}/cars/{carId}");
+				return response;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error fetching car data: {ex.Message}");
+				return null;
+			}
+		}
+
+		public async Task<List<Offer>> GetOffersAsync(int carId)
+		{
+			try
+			{
+				var response = await _httpClient.GetFromJsonAsync<List<Offer>>($"{_url}/offers/{carId}");
+				return response;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error fetching offers data: {ex.Message}");
+				return null;
+			}
+		}
+	}
+}
