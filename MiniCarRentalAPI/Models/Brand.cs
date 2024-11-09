8def1b28accefa0e7331412717858228de88f40b
DrKuba2003 <kubadrazan2@gmail.com>
2024-11-09 21:17:33 +0100

    added entityframework added models created controller scaffold created CarRentalContext

---
+++ MiniCarRentalAPI/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniCarRentalAPI.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        public ICollection<Model> Models { get; set; }
+
+    }
+}
--- MiniCarRentalAPI/Program.cs
+++ MiniCarRentalAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using MiniCarRentalAPI.Data;
+
 namespace MiniCarRentalAPI
 {
 	public class Program
@@ -13,6 +16,11 @@ namespace MiniCarRentalAPI
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+
+			// TODO Change to AddDbContextFactory??
+			builder.Services.AddDbContext<CarRentalContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+				);
 
 			var app = builder.Build();
 
