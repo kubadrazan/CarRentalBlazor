7d5d03a8aca8adc303170a5bf733ab7a03d8a825
p10tr13 <160889621+p10tr13@users.noreply.github.com>
2024-12-28 15:45:19 +0100

    Added adding good header with apiKey to request

---
+++ MiNICarRentalBrowser/ApiKeyProvider.cs
@@ -0,0 +1,27 @@
+﻿using MiNICarRentalBrowser.Data;
+
+namespace MiNICarRentalBrowser
+{
+    public class ApiKeyProvider
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _apiA;
+
+        public ApiKeyProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _apiA = configuration.GetValue<string>("ApiUrls:ApiRentalA");
+        }
+
+        public string GetApiKeyAsync(string apiUrl)
+        {
+            string keyName = string.Empty;
+            if (_apiA == apiUrl)
+                keyName = "aApiKey";
+            else
+                throw new KeyNotFoundException($"No key found for Url {apiUrl}");
+
+            return _configuration[keyName];
+        }
+    }
+}
