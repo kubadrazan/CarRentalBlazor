c01cae53c04e6584c4083a2d2a5ca484486be90a
p10tr13 <160889621+p10tr13@users.noreply.github.com>
2025-01-06 10:54:41 +0100

    deleted useless api endpoints

--- MiniCarRentalAPI/Controllers/CarsController.cs
+++ MiniCarRentalAPI/Controllers/CarsController.cs
@@ -119,46 +119,5 @@ namespace MiniCarRentalAPI.Controllers
 
             return Ok(result);
         }
-
-        // POST: api/Cars
-        [HttpPost]
-        public async Task<ActionResult<Car>> PostCar(Car car)
-        {
-            _context.Cars.Add(car);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetCar), new { id = car.ID }, car);
-        }
-
-        // PUT: api/Cars/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCar(int id, Car car)
-        {
-            if (id != car.ID)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(car).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        // DELETE: api/Cars/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCar(int id)
-        {
-            var car = await _context.Cars.FindAsync(id);
-            if (car == null)
-            {
-                return NotFound();
-            }
-
-            _context.Cars.Remove(car);  // Usunięcie samochodu z bazy danych
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
     }
 }
