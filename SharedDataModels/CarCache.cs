7b37e93c1cbdae518b469283dbb94851f518e006
DrKuba2003 <kubadrazan2@gmail.com>
2025-01-08 00:01:34 +0100

    Cars cache table added

---
+++ SharedDataModels/CarCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDataModels
+{
+    public class CarCache
+    {
+        public int ID { get; set; }
+
+        public string BrandName { get; set; }
+
+        public string ModelName { get; set; }
+
+        public int ProductionYear { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DownloadedDate { get; set; }
+
+        public int SourceApiID { get; set; }
+    }
+}
