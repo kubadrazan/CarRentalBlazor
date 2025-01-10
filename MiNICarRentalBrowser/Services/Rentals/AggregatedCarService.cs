1ad30d44984bcba18a4e07ef60832f88802f52ad
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2025-01-10 14:57:27 +0100

    Basic integrations

--- MiNICarRentalBrowser/Services/Rentals/AggregatedCarService.cs
+++ MiNICarRentalBrowser/Services/Rentals/AggregatedCarService.cs
@@ -70,63 +70,63 @@ namespace MiNICarRentalBrowser.Services.Car_Service
 		}
 
 		public Task<Car> GetCarDetailsAsync(int carId)
-		{
+		{// TODO multipleApi
 			return _carRentalServiceFactory.GetService(0).GetCarDetailsAsync(carId);
 		}
 
-		public Task<List<Offer>> GetOffersAsync(int carId)
+		public Task<List<Offer>> GetOffersAsync(int carId, int apiId)
 		{
-			return _carRentalServiceFactory.GetService(0).GetOffersAsync(carId);
+			return _carRentalServiceFactory.GetService(apiId).GetOffersAsync(carId);
 		}
 
 		public Task<string> ConfirmOffer(Guid offerId)
-		{
+		{ // TODO multipleApi
 			return _carRentalServiceFactory.GetService(0).ConfirmOffer(offerId);
 		}
 
-		public Task<string> ChooseOffer(int offerid, string emailAddress)
-		{
-			return _carRentalServiceFactory.GetService(0).ChooseOffer(offerid, emailAddress);
+		public Task<string> ChooseOffer(int offerid, string emailAddress, int apiID)
+		{ 
+			return _carRentalServiceFactory.GetService(apiID).ChooseOffer(offerid, emailAddress);
 		}
 
 		public Task<Rental> GetRentalAsync(RentalBrowser rentalBrowser)
-		{
+		{ // TODO Not Used??
 			return _carRentalServiceFactory.GetService(0).GetRentalAsync(rentalBrowser);
 		}
 
 		public Task<Rental> GetRentalAsync(int Id)
-		{
+		{ // TODO multipleApi
 			return _carRentalServiceFactory.GetService(0).GetRentalAsync(Id);
 		}
 
-		public Task<byte[]> GetCarImage(int Id)
-		{
+		public Task<byte[]> GetCarImage(int Id) 
+		{ // TODO Not used??
 			return _carRentalServiceFactory.GetService(0).GetCarImage(Id);
 		}
 
 		public Task<int> GetRentalsCountAsync()
-		{
+		{ // TODO multipleApi
 			return _carRentalServiceFactory.GetService(0).GetRentalsCountAsync();
 		}
 
 		public Task<List<Rental>> GetRentalsAsync(int? pageInd, int pageSize = 15)
-		{
+		{ // TODO multipleApi
 			return _carRentalServiceFactory.GetService(0).GetRentalsAsync(pageInd, pageSize);
 		}
 
 		public Task ReturnCarAsync(Rental rental)
 		{
-			return _carRentalServiceFactory.GetService(0).ReturnCarAsync(rental);
+			return _carRentalServiceFactory.GetService(rental.SourceAPI).ReturnCarAsync(rental);
 		}
 
 		public Task AcceptCarReturn(int rentalId, string employeeEmail, string acceptationDescription, string carImage)
-		{
+		{ // TODO multipleApi
 			return _carRentalServiceFactory.GetService(0).AcceptCarReturn(rentalId, employeeEmail, acceptationDescription, carImage);
 		}
 
-		public Task<byte[]> GetImage(int rentalId)
+		public Task<byte[]> GetImage(Rental rental)
 		{
-			return _carRentalServiceFactory.GetService(0).GetImage(rentalId);
+			return _carRentalServiceFactory.GetService(rental.SourceAPI).GetImage(rental.ID);
 		}
 	}
 }
