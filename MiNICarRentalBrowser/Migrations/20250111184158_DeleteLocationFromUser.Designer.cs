d8247757c5ebc1f1041b823c228f15672975e21a
DrKuba2003 <kubadrazan2@gmail.com>
2025-01-11 19:42:27 +0100

    deleted location from user

--- MiNICarRentalBrowser/Migrations/20250111184158_DeleteLocationFromUser.Designer.cs
+++ MiNICarRentalBrowser/Migrations/20250111184158_DeleteLocationFromUser.Designer.cs
@@ -4,6 +4,7 @@ using MiNICarRentalBrowser.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MiNICarRentalBrowser.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20250111184158_DeleteLocationFromUser")]
+    partial class DeleteLocationFromUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
