922a10ec4274173a197c34fdee6cbfb0cd3898c6
DrKuba2003 <kubadrazan2@gmail.com>
2025-01-11 19:06:33 +0100

    corrected adding rental browser to table

--- MiNICarRentalBrowser/Services/Rentals/CarRentalA.cs
+++ MiNICarRentalBrowser/Services/Rentals/CarRentalA.cs
@@ -76,8 +76,6 @@ namespace MiNICarRentalBrowser.Services.Car_Service
 			try
 			{
 				var response = await _httpClient.PutAsJsonAsync<Guid>($"{_apiUrl}/api/Rental/offers/acceptOffer", offerId);
-				await _userService.AddRentalAsync(await response.Content.ReadFromJsonAsync<Rental>());
-
 				return await response.Content.ReadAsStringAsync();
 			}
 			catch (Exception ex)
@@ -96,8 +94,9 @@ namespace MiNICarRentalBrowser.Services.Car_Service
 				if (!response.IsSuccessStatusCode)
 				{
 					return "Error";
-				}
-				return await response.Content.ReadAsStringAsync();
+                }
+                await _userService.AddRentalAsync(await response.Content.ReadFromJsonAsync<Rental>());
+                return await response.Content.ReadAsStringAsync();
 			}
 			catch (Exception ex)
 			{
