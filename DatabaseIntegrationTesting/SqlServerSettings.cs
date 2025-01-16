1cf27c9b38bba09d92f999f39528b5ae495d3be3
DrKuba2003 <kubadrazan2@gmail.com>
2025-01-16 20:15:05 +0100

    added Database Integration Tests

---
+++ DatabaseIntegrationTesting/SqlServerSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DatabaseIntegrationTesting
+{
+    public static class SqlServerSettings
+    {
+        public const string Username = "NewSA";
+        public const string Password = "Password123123!";
+        public const string Host = "localhost";
+        public const string ConnectionString = "Password=Password123123!;Persist Security Info=True;User ID=NewSA;Initial Catalog=master;Data Source=localhost;Integrated Security = True; Encrypt=True;Trust Server Certificate=True";
+    }
+}
